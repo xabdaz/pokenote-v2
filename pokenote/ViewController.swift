e25e4ca99d16f3c3e4cb8999bdcbb08c4c0a1608
Developer Xabdaz <developer@xabdaz.tech>
2022-01-22 00:01:01 +0700

    Initial Commit

---
+++ pokenote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokenote
+//
+//  Created by Developer Xabdaz on 22/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
